1d45169382020d84702bdb67e782957981889fc3
Fabian Berg <info@fabian-berg.de>
2013-11-12 23:58:58 +0100

    added forms functionality

--- main/java/com/gamepad/lib/forms/controls/Button.java
+++ main/java/com/gamepad/lib/forms/controls/Button.java
@@ -1,26 +1,34 @@
 package com.gamepad.lib.forms.controls;
 
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 
 import com.gamepad.lib.forms.Control;
 import com.gamepad.lib.forms.Point;
+import com.gamepad.lib.forms.Size;
 
 /**
  * Created by Fabian on 12.11.13.
  */
-public class Button extends Control
+public abstract class Button extends Control
 {
-
-    @Override
-    public void onClick(Point point)
+    public Button(Point location, Size size)
     {
-
+        super(location, size);
     }
 
     @Override
     public void draw(Canvas canvas)
     {
-        
+        float x = getRectangle().getX();
+        float y = getRectangle().getY();
+        float width = getRectangle().getWidth();
+        float height = getRectangle().getHeight();
+        Paint p = new Paint();
+        p.setARGB(0, 0, 0, 0);
+        canvas.drawRect(x, y, width, height, p);
     }
 
     @Override
