416f92de037ee92accdedea4f337a1f5debe5db1
Fabian Berg <info@fabian-berg.de>
2013-11-12 20:55:07 +0100

    commented the auto updater class

--- main/java/com/gamepad/lib/update/AutoUpdater.java
+++ main/java/com/gamepad/lib/update/AutoUpdater.java
@@ -22,16 +22,21 @@ import java.util.Iterator;
  */
 public class AutoUpdater {
 
+    //the url to the info file (saved as a json file)
     public static final String INFO_FILE = "https://dl.dropboxusercontent.com/u/19280458/updateInfo.json";
+
+    //the url used for an internet check
     public static final String INTERNET_CHECK_URL = "http://google.de";
 
     private ArrayList<AvailableGame> games;
 
+    //initialized a new instance of the autoupdater class
     public AutoUpdater()
     {
         games = new ArrayList<AvailableGame>();
     }
 
+    //downloads via the http protocol the page source to a string
     private static String downloadHttp(URL url) throws IOException {
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
         connection.setRequestMethod("GET");
@@ -65,12 +70,13 @@ public class AutoUpdater {
         return result;
     }
 
+    //gets all the games that are currently in the game list
     public ArrayList<AvailableGame> getGames()
     {
         return games;
     }
 
-    //
+    //get the data for the auto updater class including all games for the core
     public void getData()
     {
         try{
@@ -85,6 +91,8 @@ public class AutoUpdater {
 
             //convert the json array to an arraylist with json objects
             ArrayList<JSONObject> jObjects = getArrayListFromJsonArray(jArr);
+
+            //walk through the elements and add new available games to the list in this class
             for(JSONObject jObject : jObjects)
             {
                 String name = jObject.getString("Name");
@@ -104,6 +112,7 @@ public class AutoUpdater {
         }
     }
 
+    //Checks if there is an internet connection
     public Boolean hasInternetConnection()
     {
         try
