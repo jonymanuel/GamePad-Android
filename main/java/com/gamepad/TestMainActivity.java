4f24f3ad3f80ce4c1e45275d4e6fde72c9084bee
jordisipkens <jordisipkens@gmail.com>
2013-12-13 12:10:55 +0100

    Herstellen originele document

---
+++ main/java/com/gamepad/TestMainActivity.java
@@ -0,0 +1,17 @@
+package com.gamepad;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+
+
+public class TestMainActivity extends AndroidApplication {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+}
+
