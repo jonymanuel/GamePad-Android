b920cc4bef2e691d2451ce1fdf5e03ee6e09f15d
jordisipkens <jordisipkens@gmail.com>
2014-01-08 09:15:39 +0100

    CHANGED layout table

--- main/java/com/gamepad/lib/game/NewRound.java
+++ main/java/com/gamepad/lib/game/NewRound.java
@@ -13,9 +13,7 @@ import com.gamepad.lib.carddealer.ImageParser;
  */
 public class NewRound extends Activity{
 
-    ImageView imgCard1, imgCard2, imgCard3, imgCard4, imgCard5, imgPlayer1_1, imgPlayer1_2, imgPlayer2_1, imgPlayer2_2, imgPlayer3_1,
-            imgPlayer3_2, imgPlayer4_1, imgPlayer4_2, imgPlayer5_1, imgPlayer5_2, imgPlayer6_1, imgPlayer6_2, imgPlayer7_1, imgPlayer7_2,
-            imgPlayer8_1, imgPlayer8_2;
+    ImageView imgFlop1, imgFlop2, imgFlop3, imgTurn, imgRiver;
     private ImageParser imageParser;
 
     LinearLayout mLinearLayout;
@@ -23,29 +21,14 @@ public class NewRound extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        imgCard1 = (ImageView) findViewById(R.id.flop1);
-        imgCard2 = (ImageView) findViewById(R.id.flop2);
-        imgCard3 = (ImageView) findViewById(R.id.flop3);
-        imgCard4 = (ImageView) findViewById(R.id.turn);
-        imgCard5 = (ImageView) findViewById(R.id.river);
-        imgPlayer1_1 = (ImageView) findViewById(R.id.card1_1);
-        imgPlayer1_2 = (ImageView) findViewById(R.id.card1_2);
-        imgPlayer2_1 = (ImageView) findViewById(R.id.card2_1);
-        imgPlayer2_2 = (ImageView) findViewById(R.id.card2_2);
-        imgPlayer3_1 = (ImageView) findViewById(R.id.card3_1);
-        imgPlayer3_2 = (ImageView) findViewById(R.id.card3_2);
-        imgPlayer4_1 = (ImageView) findViewById(R.id.card4_1);
-        imgPlayer4_2 = (ImageView) findViewById(R.id.card4_2);
-        imgPlayer5_1 = (ImageView) findViewById(R.id.card5_1);
-        imgPlayer5_2 = (ImageView) findViewById(R.id.card5_2);
-        imgPlayer6_1 = (ImageView) findViewById(R.id.card6_1);
-        imgPlayer6_2 = (ImageView) findViewById(R.id.card6_2);
-        imgPlayer7_1 = (ImageView) findViewById(R.id.card7_1);
-        imgPlayer7_2 = (ImageView) findViewById(R.id.card7_2);
-        imgPlayer8_1 = (ImageView) findViewById(R.id.card8_1);
-        imgPlayer8_2 = (ImageView) findViewById(R.id.card8_2);
+        imgFlop1 = (ImageView) findViewById(R.id.flop1);
+        imgFlop2 = (ImageView) findViewById(R.id.flop2);
+        imgFlop3 = (ImageView) findViewById(R.id.flop3);
+        imgTurn = (ImageView) findViewById(R.id.turn);
+        imgRiver = (ImageView) findViewById(R.id.river);
 
 
+        setContentView(R.layout.activity_round);
 
     }
 }
