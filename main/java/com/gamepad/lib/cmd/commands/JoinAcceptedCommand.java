1fa72808a98d0b05a5bb56c1cf921eb678ead062
Fabian Berg <info@fabian-berg.de>
2014-01-10 13:02:16 +0100

    ADDED more commands + bug fixes

---
+++ main/java/com/gamepad/lib/cmd/commands/JoinAcceptedCommand.java
@@ -0,0 +1,29 @@
+package com.gamepad.lib.cmd.commands;
+
+import android.util.Log;
+
+import com.gamepad.lib.GPC;
+import com.gamepad.lib.cmd.ICommand;
+import com.gamepad.lib.game.Lobby;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Fabian on 10.01.14.
+ */
+public class JoinAcceptedCommand implements ICommand {
+
+    @Override
+    public String getCommandString() {
+        return "joinaccepted";
+    }
+
+    @Override
+    public Boolean runCommand(JSONObject input) throws Exception {
+        String joinedLobby = input.getString("joinedlobby");
+        Lobby theLobby = GPC.getJoin().getLobbyByName(joinedLobby);
+        GPC.getJoin().setCurrentLobby(theLobby);
+        Log.d("JoinAcceptedCommand", "The join got accepted. JoinedLobby: " + joinedLobby);
+        return true;
+    }
+}
---
+++ main/java/com/gamepad/lib/game/Mode.java
@@ -0,0 +1,9 @@
+package com.gamepad.lib.game;
+
+/**
+ * Created by Fabian on 10.01.14.
+ */
+public interface Mode {
+    void initMode();
+    void clearMode();
+}
